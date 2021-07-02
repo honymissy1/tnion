b6df46e34f9e9fabdf7da951963d9a026edfe26b
honymissy1 <owolewaonimisi@gmail.com>
2021-07-02 17:03:04 +0100

    Let's make the server

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const registerUser = require('./routes/registeredUser')
 require('dotenv').config()
 
-
-
 const app = express()
 const port = process.env.PORT
 app.listen(port);
 
 
 app.use('/home', registerUser)
+app.get('/', (req,res) =>{
+    res.send('booom')
+})
