9c01c3045ccf103a4d65b63ae5d6bcbbd178296f
honymissy1 <owolewaonimisi@gmail.com>
2021-07-02 17:28:26 +0100

    Allowing Cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const registerUser = require('./routes/registeredUser')
 require('dotenv').config()
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT
 app.listen(port);
-
+app.use(cors());
 
 app.use('/home', registerUser)
 app.get('/', (req,res) =>{
